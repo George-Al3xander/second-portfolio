bee8649b3cc086345dfb4aeb2c6f9d14aaf2545f
George <george.valuiskyi@gmail.com>
2025-01-09 11:56:58 +0200

    test: update jest config
    Add moduleNameWrapper to the jest config file.

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,19 @@
 import type { Config } from "jest";
 import nextJest from "next/jest.js";
+import { pathsToModuleNameMapper } from "ts-jest";
+import { compilerOptions } from "./tsconfig.json";
 const createJestConfig = nextJest({
-  dir: "./",
+    dir: "./",
 });
 
 const config: Config = {
-  coverageProvider: "v8",
-  testEnvironment: "jest-environment-jsdom",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-  preset: "ts-jest",
+    coverageProvider: "v8",
+    testEnvironment: "jest-environment-jsdom",
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+    preset: "ts-jest",
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+        prefix: "<rootDir>/",
+    }),
 };
 
 export default createJestConfig(config);
